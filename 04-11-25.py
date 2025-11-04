11ab4ff7f7dda29b0e5ee8a6778c4a204b1cdbca
PietroBrunoEinstein <pietro.bruno@liceoeinsteinmilano.edu.it>
2025-11-04 12:47:30 +0100

    Update 04-11-25.py

--- 04-11-25.py
+++ 04-11-25.py
@@ -1,8 +1,9 @@
 def principale():
     nome = input("Inserisci il tuo nome: ")
     età = int(input("Inserisci la tua età: "))
+    stringa = f"Lunghezza della stringa insufficiente.\nLa stringa deve essere almeno di quattro caratteri,\nma è di {len(nome)}."
     if len(nome) < 4:
-        print(f"Lunghezza della stringa insufficiente\nLa stringa deve essere almeno di quattro caratteri,\nma è di {len(nome)}")
+        print(stringa)
         return
     if età % 3 == 0 and età % 2 != 0:
         stringa = nome[:2] + nome[-2:]
@@ -16,3 +17,4 @@ try:
 except Exception as e:
 
     print(f"Si è verificato un errore: '{e}'.")
+
